7b31b843a173249a56c9cc85f074b0b82af75e79
siltonIT <140279319+siltonIT@users.noreply.github.com>
2023-12-20 00:06:24 +0300

    fix

--- Task 3.3/Task 3.3.cpp
+++ Task 3.3/Task 3.3.cpp
@@ -6,7 +6,31 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-//Функция находит максимальную степень двойки 
+int find_pow_two(int);
+
+
+int find_amount_zero(int number)
+{
+	int counter = 0;
+	int pow_two = find_pow_two(number);
+
+	while (number > 0)
+	{
+		if (number >= pow_two)
+		{
+			number -= pow_two;
+		}
+		else
+		{
+			++counter;
+		}
+
+		pow_two = pow_two >> 1;
+	}
+
+	return counter;
+}
+
 int find_pow_two(int number)
 {
 	int pow_two = 1;
@@ -19,43 +43,6 @@ int find_pow_two(int number)
 	return (pow_two >> 1);
 }
 
-//Функция находит простое число меньшее нашего с помощью решета Эратосфена
-bool is_prime_number(int number, int prime_number)
-{
-	const int SIZE = 10000;
-	int prime_numbers[SIZE];
-
-	for (int i = 0; i < number; ++i)
-	{
-		prime_numbers[i] = i + 1;
-	}
-
-	for (int i = 2; i < number; ++i)
-	{
-		for (int n = i*i - 1; n < number; ++n)
-		{
-			if (prime_numbers[n] % i == 0)
-			{
-				prime_numbers[n] = 0;
-			}
-		}
-	}
-
-	for (int i = 0; i < number; ++i)
-	{
-		if (prime_numbers[i] == 0)
-		{
-			continue;
-		}
-
-		if (prime_number == prime_numbers[i])
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
 
 int main()
 {
@@ -65,12 +52,36 @@ int main()
 	int pow_two = find_pow_two(number);
 	int prime_number;
 
-	for (int i = pow_two + 1; i <= number; ++i)
+	const int SIZE = 2000;
+	int prime_numbers[SIZE];
+
+	for (int i = 0; i < number; ++i)
 	{
-		if (is_prime_number(number, i))
+		prime_numbers[i] = i + 1;
+	}
+
+	for (int i = 2; i < number; ++i)
+	{
+		for (int n = i * i - 1; n < number; ++n)
 		{
-			prime_number = i;
-			break;
+			if (prime_numbers[n] % i == 0)
+			{
+				prime_numbers[n] = 0;
+			}
+		}
+	}
+
+	int max_am_zero = find_amount_zero(prime_numbers[pow_two]);
+	prime_number = prime_numbers[pow_two];
+
+	for (int i = pow_two + 1; i < number; ++i)
+	{
+		int am_zero = find_amount_zero(prime_numbers[i]);
+		
+		if (max_am_zero < am_zero)
+		{
+			max_am_zero = am_zero;
+			prime_number = prime_numbers[i];
 		}
 	}
 
